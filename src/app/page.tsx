75196edc6350d4c7f8bdf6f86cb4c7f08e4bec31
cheekean.chin <cheekean.chin@ufinity.com>
2025-03-05 10:55:02 +0800

    Added the version number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-import { Button, Center, Container, Group, PasswordInput } from "@mantine/core";
+import {
+  Button,
+  Center,
+  Container,
+  Group,
+  PasswordInput,
+  Text,
+} from "@mantine/core";
 import { setPasswordCookie } from "./actions";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/main");
-  // return (
-  //   <Container size="sm">
-  //     <Center h="100vh">
-  //       <form action={setPasswordCookie}>
-  //         <Group align="end">
-  //           <PasswordInput name="talia-password" w={300} label="Password" />
-  //           <Button type="submit">Enter</Button>
-  //         </Group>
-  //       </form>
-  //     </Center>
-  //   </Container>
-  // );
+  // redirect("/main");
+  return (
+    <Container size="sm">
+      <Center h="100vh">
+        <form action={setPasswordCookie}>
+          <Group align="end">
+            <PasswordInput name="talia-password" w={300} label="Password" />
+            <Button type="submit">Enter</Button>
+          </Group>
+        </form>
+      </Center>
+
+      <Center mt="md">
+        <Text size="sm" color="gray">
+          Version 0.0.1
+        </Text>
+      </Center>
+    </Container>
+  );
 }
